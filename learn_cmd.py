bafa2891ebbe79c585fbc4f130dc596a0c96a900
stephen-lakes <oluyomiolamilekan99@gmail.com>
2021-06-13 23:49:00 +0100

    modified learn_cmd.py

--- learn_cmd.py
+++ learn_cmd.py
@@ -1,33 +1,120 @@
 import random
 
+print("=================================")
+print("COMMAND LINE CRASH COUSRSE")
+print("=================================")
 
-game_commands = {'q': 'quit', 'h': 'hint', 's' : 'start over' }
+game_commands = {'Q': 'quit', 'H': 'hint', 'M' : 'main menu' }
 
+PHARASES = {
+    "mkdir %%%":
+        "Make a folder named %%% in the current folder",
+    "rmdir %%%":
+        "Remove/Delete the folder named %%% ",
+    "cd %%%":
+        "Navigate to the %%% directory",
+    "touch ***":
+        "Create a file named ***in the current folder",
+    "rm *** ***":
+        "Remove the following files ***, *** from the current directory",
+    "unlink ***":
+        "Delete the *** file",
+    "clear":
+        "Clear the terminal or command line",
+    "ls":
+        "List all the files in the current directory",
+    "cd ..":
+        "Navigate a directory updward, closer to the root directory",
+    
+    
+}
 
-words = open('words.txt', 'r')
+WORDS_FILE = open('words.txt', 'r')
+WORDS = []
 
-for word in words:
-    print(word)
+for word in WORDS_FILE:
+    #WORDS.append(str(word.strip(), encoding="utf-8"))
+    WORDS.append(word)
+
+def convert(snippet, phrase):
+
+    folder_names = [ w.capitalize() for w in random.sample(WORDS, snippet.count("%%%")) ]
+    file_names = random.sample(WORDS, snippet.count("***"))
+    results = []
+
+    for sentence in snippet, phrase:
+        result = sentence[:]
+
+        # fake folder names 
+        for word in folder_names:
+            result = result.replace("%%%", word, 1)
+
+        # fake file names
+        for word in file_names:
+            result = result.replace("***", word, 1)
+
+        results.append(result)
+    
+    return results
 
 
 condition = True
-question = "random question"
-answer = "random question"
-
 
 while condition:
-    
-    print(question)
-    
-    ans = input("answer to the question: ")
 
-    if ans == answer:
-        print("correct")
+    print("Type 'C' to view game commands")
+
+
+    command = input("> ")
+
+    if command.lower() == "c":
+
+        for command_key in game_commands:
+            print(f" {command_key} for {game_commands[command_key]} ")
+
+    elif command.lower() == 'm':
+
+        print("======== MAIN MENU ==============")
         
-    elif ans == 'q':
-        print("Exiting the shell")    
-        break
-    
+        print("Choose 1 for the basic, choose 2 for intermediat, choose 3 for advance")
+
+        level = input("> ")
+
+        if level == "1":
+            while True:
+
+                snippets = list(PHARASES.keys())
+                random.shuffle(snippets)
+
+                for snippet in snippets:
+                    phrase = PHARASES[snippet]
+
+                    answer, question = convert(snippet, phrase)
+
+                    print(question)
+
+                    input("> ")
+                    print(f"ANSWER: {answer}\n\n")
+
+
+
+                
+
+
+
+        
+        else:
+            print("INVALID COMMAND")
+
+    elif command.lower() == "q":
+
+        print("Are you sure you want to exit")
+        confirm = input(" 'y' for yes 'n' for no > ")
+        if confirm.lower() == 'y':
+            break
+
     else:
-        print("Incorrect")
-    
+
+        print("INVALID COMMAND")
+
+        
